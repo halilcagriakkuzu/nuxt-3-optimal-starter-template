7b9f022f67953f7a7fa7769922308ff4b81c4e5c
Halil Çağrı AKKUZU <halilcagriakkuzu@gmail.com>
2023-01-17 20:45:05 +0300

    feat: example tests

---
+++ tests/example.test.ts
@@ -0,0 +1,12 @@
+import { assert, describe, expect, it } from 'vitest'
+
+describe('suite name', () => {
+  it('foo', () => {
+    expect(1 + 1).toEqual(2)
+    expect(true).to.be.true
+  })
+
+  it('bar', () => {
+    assert.equal(Math.sqrt(4), 2)
+  })
+})
