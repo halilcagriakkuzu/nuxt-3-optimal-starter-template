4dea05450e4178de218aaa848fd5895257f6c4e2
Halil Çağrı AKKUZU <halilcagriakkuzu@gmail.com>
2023-01-17 20:13:10 +0300

    feat: prettier plugins for tailwind and importOrder

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello world from project lupus!</h1>
+     <h1 class="underline  text-3xl font-bold pl-2">Hello world from project lupus!</h1>
   </div>
 </template>
