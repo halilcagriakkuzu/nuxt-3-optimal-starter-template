6edb832e51b60f8c0a37b5d4dd8bbe71f198b72b
Halil Çağrı AKKUZU <halilcagriakkuzu@gmail.com>
2023-01-17 20:15:01 +0300

    test: prettier2

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="pl-2 text-3xl font-bold underline">Hello world from project lupus!</h1>
+    <h1 class="pl-3 text-3xl font-bold underline">Hello world from project lupus!</h1>
   </div>
 </template>
