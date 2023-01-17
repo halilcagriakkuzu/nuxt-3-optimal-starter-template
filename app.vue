f1aaac3a034910295ed9a36d3f7709c1a2cebcb9
Halil Çağrı AKKUZU <halilcagriakkuzu@gmail.com>
2023-01-17 20:27:30 +0300

    feat: prettier plugin organize attributes for vue

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1 class="pl-3 text-3xl font-bold underline">Hello world from project lupus!</h1>
-    <h1 class="pl-3 text-3xl font-bold underline">Hello world from project lupus!</h1>
+    <h1 v-if="true" class="pl-3 text-3xl font-bold underline">Hello world from project lupus!</h1>
   </div>
 </template>
